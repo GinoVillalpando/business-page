dc3c925d185aab43dfe0b6ba4702103dfc7ba07e
Gino Villalpando <grievxus@outlook.com>
2020-05-05 20:38:06 -0600

    added some better descriptive comments to the code

--- public_html/js/check.js
+++ public_html/js/check.js
@@ -1,15 +1,19 @@
 document.addEventListener('change', function(event) {
+
+    // variables that hold our event targets and values along with getting elements from DOM and manipulation
     let size = event.target;
     let item = event.target.value
     let textarea = document.getElementById("message")
     let text = document.createTextNode("I want to buy: " + item + " size: " + size.name + " ");
 
     if(size.checked) {
-        // Checkbox is checked..
+
+        // if Checkbox is checked..
         let textNode = textarea.appendChild(text).innerHTML;
 
     } else {
-        // if the checkbox has been unchecked then reloads the location of the page
+
+        // else the checkbox has been unchecked then reloads the location of the page
         this.location.reload(true);
 
     }
